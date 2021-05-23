c5255a599e6eda83f9805c29657c927a99bb428a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-23 18:22:23 +0200

    Prepare helper functions for lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,9 +1,49 @@
-class Lexer:
-    def __init__(self, tokens):
-        self.tokens = tokens
+import typing
 
-    def tokenize(self, code):
+from lexer.token import Token
+
+
+class Lexer:
+    def __init__(self, tokens: typing.List[Token]) -> None:
+        self.tokens = tokens
+        self.code = ''
+        self.position = 0
+        self.read_position = 0
+        self.current_char = ''
+
+    def tokenize(self, code: str) -> typing.List[Token]:
+        self.code = code
+        while self.current_char != '\0':
+            self._read_char()
+            print(self.current_char)
+
         return []
+
+    def _next_token(self) -> Token:
+        self._skip_whitespace()
+
+        token = None
+
+        # TODO: Detect type of token
+        # TODO: Create Token
+
+        self._read_char()
+
+        return token
+
+    def _read_char(self) -> str:
+        if self.read_position >= len(self.code):
+            self.current_char = '\0'
+        else:
+            self.current_char = self.code[self.read_position]
+
+        self.position = self.read_position
+        self.read_position += 1
+
+    # TODO: Make this configurable
+    def _skip_whitespace(self):
+        while self.current_char in [' ', '\t', '\r', '\n']:
+            self._read_char()
 
 
 class LexerError(Exception):
